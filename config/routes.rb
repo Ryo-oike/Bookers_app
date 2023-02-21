feb8108af6b0382feba7eb8a9ad508430efd4702
ryooike <120473628+Ryo-oike@users.noreply.github.com>
2023-02-21 16:58:51 +0000

    ver1.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'books' => 'books#index'
+  get 'show' => 'books#show'
+  get 'edit' => 'books#edit'
+  get 'top' => 'homes#top'
+  get 'new' => 'books#new'
+  #resources :books
+  post 'books' => 'books#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
